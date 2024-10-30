3186f7c3a2340ff83a4bce7258bfffbdcbc07b3d
misha-chertushkin <chertushkin@google.com>
2024-10-30 19:53:52 +0000

    Remove ignored files

--- android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.mycompany.mishaduplicate
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/custom_code/actions/index.dart
+++
@@ -1,3 +0,0 @@
-export 'login_with_google.dart' show loginWithGoogle;
-export 'logout_google.dart' show logoutGoogle;
-export 'document_picker.dart' show documentPicker;
